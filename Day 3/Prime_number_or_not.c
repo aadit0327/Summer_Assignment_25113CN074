06d55742479f0c5743e24ca1b96a5ae3e6d76c6f
aadit0327 <25113cn074@glbitm.ac.in>
2026-05-30 14:32:19 +0530

    Create Prime_number_or_not.c

---
+++ Day 3/Prime_number_or_not.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() 
+{
+    int n, i, count = 0;
+    printf("Enter any positive integer: ");
+    scanf("%d", &n);
+    if (n <= 1) 
+    {
+        count = 1; 
+    } 
+    else 
+    {
+        for (i = 1; i <= n; i++) 
+        {
+            if (n % i == 0) 
+            {
+                count++;
+            }
+        }
+    }
+    if (count == 2)
+        printf("%d is a prime number.", n);
+    else
+        printf("%d is not a prime number.", n);
+
+    return 0;
+}
