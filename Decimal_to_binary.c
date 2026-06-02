01a168371908d9cabe2152478897fb766ee9a6d9
aadit0327 <25113cn074@glbitm.ac.in>
2026-06-02 16:11:53 +0530

    Create Decimal_to_binary.c

---
+++ Decimal_to_binary.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() 
+{
+    int n, i = 0, j, r[32];
+    printf("Enter any decimal number: ");
+    scanf("%d", &n) 
+
+    if (n == 0) 
+    {
+        printf("Binary number = 0\n");
+        return 0;
+    }
+
+    while (n > 0) 
+    {
+        r[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+
+    printf("Binary number = ");
+    for (j = i - 1; j >= 0; j--) 
+    {
+        printf("%d", r[j]);
+    }
+    printf("\n");
+
+    return 0;
+}
