b404fdb3366e4f66e76721b33d6f1952799e9d35
aadit0327 <25113cn074@glbitm.ac.in>
2026-06-12 13:43:47 +0530

    Create Rotate_array_right.c

---
+++ Day 15/Rotate_array_right.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+  int i, n, a[100], temp;
+  printf("Enter the number of elements of array upto 100 only\n");
+  printf("Enter the number of elements of array : ");
+  scanf("%d", &n);
+  printf("\nEnter the elements of array - \n");
+  for(i=0; i<n; i++)
+    {
+       printf("Enter a[%d] = ", i);
+       scanf("%d", &a[i]);
+    }
+     printf("\nEntered Array - ");
+    for(i=0; i<n; i++)
+      {
+         printf(" %d", a[i]);
+      }
+      printf("\n");
+      
+      temp = a[n-1]; 
+      for(i = n-1; i > 0; i--) 
+      {
+         a[i] = a[i - 1];
+      }
+       a[0] = temp; 
+       printf("\nArray after right rotation - ");
+       for(i = 0; i < n; i++) 
+       {
+          printf(" %d ", a[i]);
+       }
+        printf("\n");
+       return 0;
+}
+
+
