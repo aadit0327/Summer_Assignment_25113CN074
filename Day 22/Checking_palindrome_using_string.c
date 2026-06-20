55f7a12e686e4384b75e8bf7a3ac99d53dd4638c
aadit0327 <25113cn074@glbitm.ac.in>
2026-06-20 15:36:56 +0530

    Create Checking_palindrome_using_string.c

---
+++ Day 22/Checking_palindrome_using_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[100];
+    int i, len, Palindrome = 1;
+
+    printf("Enter any string : ");
+    scanf("%s", str);
+
+    len = strlen(str);
+
+    for (i = 0; i < len / 2; i++) 
+    {
+        if (str[i] != str[len - 1 - i]) 
+        {
+            Palindrome = 0;
+            break;
+        }
+    }
+
+    if (Palindrome == 1)
+      printf("(%s) is a palindrome.\n", str);
+    else
+      printf("(%s) is not a palindrome.\n", str);
+
+    return 0;
+}
+
