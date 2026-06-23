dfdb023612bae5cf0df17b09384fb4bec5354ee1
aadit0327 <25113cn074@glbitm.ac.in>
2026-06-23 21:12:38 +0530

    Create Checking_string_rotation.c

---
+++ Day 24/Checking_string_rotation.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+int main() 
+{
+    char s1[100], s2[100], temp[200];
+    printf("Enter first string: ");
+    scanf("%s", s1);
+    printf("Enter second string: ");
+    scanf("%s", s2);
+    if (strlen(s1) == strlen(s2)) 
+    {
+         strcpy(temp, s1);
+        strcat(temp, s1); 
+        if (strstr(temp, s2))
+        {
+            printf("String is a rotation\n");
+        }
+        else
+        {
+            printf("Not a rotation\n");
+        }
+    } 
+    else 
+    {
+       printf("Not a rotation\n");
+    }
+    return 0;
+}
+
